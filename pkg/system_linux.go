8517c1051b87f3c8d8bce8c8c0c29aba31e46032
Erick Daniszewski <edaniszewski@gmail.com>
2019-12-20 22:20:14 -0500

    add system info for linux

---
+++ pkg/system_linux.go
@@ -0,0 +1,29 @@
+package pkg
+
+import (
+	"runtime"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// LoadSystemInfo loads the system info (os, arch, etc) into a SysInfo struct.
+func LoadSystemInfo() (SysInfo, error) {
+	stdout, stderr, err := runCommand("uname", "-srpo")
+	if err != nil {
+		errString := stderr.String()
+		if errString == "" {
+			errString = "<no output>"
+		}
+		log.Debugf("command error: %v", errString)
+		return SysInfo{}, err
+	}
+	info := toSlice(stdout.Bytes())
+
+	return SysInfo{
+		OS: runtime.GOOS,
+		Kernel: info[0],
+		KernelVersion: info[1],
+		Processor: info[2],
+		Arch: info[3],
+	}, nil
+}
